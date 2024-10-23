7cdc5340cb0b03369e59aa54ca9ef84933703cd3
Sujan Saitej <152066198+sujansaitej@users.noreply.github.com>
2024-10-23 17:44:06 +0530

    "Deleted files: Logo.png, README.md, main.html, and style.css"

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Load Navbar
+document.getElementById('navbar-placeholder').innerHTML = `
+    <nav>
+        <ul>
+            <li><a href="home.html">Home</a></li>
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="services.html">Services</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+    </nav>
+`;
+
+// Load Footer
+document.getElementById('footer-placeholder').innerHTML = `
+    <footer>
+        <p>&copy; 2024 Pullinam Aerospace Technologies. All Rights Reserved.</p>
+    </footer>
+`;
